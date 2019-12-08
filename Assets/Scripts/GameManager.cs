36c9cac117b65987baacf6ad21cd286f5d7b22b9
Olivier <o.cornet2@gmail.com>
2019-12-08 15:07:18 +0100

    Add menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+
+	public GameObject Game;
+	public GameObject Menu;
+	public GameObject Player;
+
+	public void Awake()
+	{
+		Instance = this;
+	}
+
+    void Start()
+    {
+		Game.SetActive(false);
+		Menu.SetActive(true);
+		Player.SetActive(false);
+    }
+
+	public void StartGame()
+	{
+		Menu.SetActive(false);
+		Game.SetActive(true);
+		Player.SetActive(true);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+		GameManager.Instance.StartGame();
+    }
+
+	public void Quit()
+	{
+		GameManager.Instance.QuitGame();
+	}
+}
