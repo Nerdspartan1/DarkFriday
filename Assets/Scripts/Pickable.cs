d3ca2e33e06f8ab7a12a6b57ce297b78f9497649
Olivier <o.cornet2@gmail.com>
2019-12-08 18:12:11 +0100

    Add pickable script and highlight shader

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pickable : MonoBehaviour
+{
+	private Material _highlightMaterial;
+	private float _highlightDecaySpeed = 2f;
+	private float _currentHighlightIntensity = 0f;
+
+	public float OutlineWidth = 0.01f;
+	public UnityEvent OnPickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		var renderer = GetComponent<Renderer>();
+		_highlightMaterial = new Material(renderer.material);
+		renderer.material = _highlightMaterial;
+    }
+
+	public void Highlight()
+	{
+		_currentHighlightIntensity = 1f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_highlightMaterial.SetFloat("_Outline", _currentHighlightIntensity * OutlineWidth);
+		if (_currentHighlightIntensity > 0)
+			_currentHighlightIntensity -= _highlightDecaySpeed * Time.deltaTime;
+		else
+			_currentHighlightIntensity = 0f;
+	}
+
+	public void PickUp()
+	{
+		OnPickUp.Invoke();
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	[HideInInspector]
+	public Camera FPSCamera;
+	public Text PickUpText;
+
+	public float MaxInteractDistance = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		FPSCamera = GetComponentInChildren<Camera>();
+		PickUpText.gameObject.SetActive(false);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Ray ray = new Ray(FPSCamera.transform.position, FPSCamera.transform.forward);
+		
+		if (Physics.Raycast(ray, out RaycastHit hit, MaxInteractDistance) && hit.collider.CompareTag("Interactable"))
+		{
+			PickUpText.gameObject.SetActive(true);
+			var pickable = hit.collider.GetComponent<Pickable>();
+			pickable.Highlight();
+			if (Input.GetKeyDown(KeyCode.E))
+			{
+				pickable.PickUp();
+			}
+		}
+		else
+		{
+			PickUpText.gameObject.SetActive(false);
+		}
+	}
+}
