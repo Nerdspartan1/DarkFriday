3d6fe928ee343b4f02c545c93f42b95fb72c3f9c
Olivier <o.cornet2@gmail.com>
2019-12-15 18:09:34 +0100

    Add randomized mannequins

---
+++ Assets/Scripts/StaticMannequin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticMannequin : MonoBehaviour
+{
+	private Animator _anim;
+
+	void Start()
+	{
+		_anim = GetComponent<Animator>();
+		_anim.SetInteger("pose", Random.Range(0, 4));
+	}
+}
