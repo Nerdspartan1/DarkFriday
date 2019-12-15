bb6ba8691ad0f5ba8742a05b1fb053697c9ca1a7
Eddie <52604400+DeadllySin@users.noreply.github.com>
2019-12-15 22:42:36 +0000

    Added AI Amb sounds; Added music prog; Added note pickup sfx;

---
+++ Assets/Scripts/SoundAnimat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAnimat : MonoBehaviour
+{
+    public static SoundAnimat sap;
+    // Start is called before the first frame update
+
+    void PlayAiMov()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached(SoundManager.sm.aiMov, this.gameObject);
+    }
+}
